0f54762fe3933874e0254439c0b05e3bba747852
metaverse-c <cailingp2159@163.com>
2023-04-10 01:52:37 +0800

    first connect to git repo

---
+++ src/main/java/org/example/FanCtlConfig.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class FanCtlConfig {
+    private int slot;
+    private int commtype;//0-自动档,1-手动档;
+    //private FanCtlCommType commtype;
+    private int speedmode;//0-5档
+
+}
